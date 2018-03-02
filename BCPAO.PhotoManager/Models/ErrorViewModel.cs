240822cf57c406ca2f39ac0c26f0a1e765ea05d1
James Moore <james.moore@bcpao.us>
2018-03-01 21:15:03 -0500

    Add project files.

---
+++ BCPAO.PhotoManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BCPAO.PhotoManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
